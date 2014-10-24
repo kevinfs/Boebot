e27f9e9d7ca7da93b7e58f766c8385219f550fc8
kevinfs <Jarvis@MacBook-de-Jarvis.local>
2014-10-24 18:59:41 +0200

    v1 + last modifs

--- v1/v1.ino
+++ v1/v1.ino
@@ -1,8 +1,14 @@
 #include <Servo.h>
 #include <math.h>
 
-
+const int BLACKLINES = 10;
+boolean isOnWhiteLine = true; 
+int time = 0;
+int beginTime = 0;
+int virageTime = 0;
 // LEDS
+int i = 0;
+int j = 0;
 int pinLed = 3;
 int pinLedLightLeft = 11;
 int pinLedLightRight = 10;
@@ -15,7 +21,7 @@ int pinLeftServo = 12;
 
 // RightServo
 int stopSign = 96;
-int forwardSignRight = 51;
+int forwardSignRight = 90;//51
 int forwardQuickSignRight = 0;
 //int forwardQuickerSignRight = 544;
 int backwardSignRight = 140;
@@ -23,7 +29,7 @@ int backwardQuickSignRight = 180;
 //int backwardQuickerSignRight = 543;
 
 // LeftServo
-int forwardSignLeft = 140;
+int forwardSignLeft = 100;//140
 int forwardQuickSignLeft = 180;
 //int forwardQuickerSignLeft = 543;
 int backwardSignLeft = 51;
@@ -66,35 +72,95 @@ void setup() {
   //   leftServo.write(100);
 
   //moveForwardCM(30.0);
-//   uTurn();
-//  moveToFirstBase(1,1,-20,1);
+  //   uTurn();
+  //  moveToSecondBase(1,1,-20,1);
+  beginTime=millis();
 }
 
 void loop() {
   //digitalWrite(3,HIGH);
   //  outputPhotoTransistorValues();
- followLine();
-
+  //followLine();
   // v = 11 cm/s en normal
   // v = 12 cm/s en quick
 
   //  rightServo.write(forwardQuickSignRight); 
   //    leftServo.write(forwardQuickSignLeft);
+  
+  /*int x1, x2, y1, y2;
+  x1=-80;
+  y1=110;
+  x2=20;
+  y2=80;
+  moveToSecondBase(x1, y1, x2, y2);*/
+  //moveToThirdBase();
+  //moveToFourthBase();
+  //moveToFifthBase();
+  moveToSixthBase();
+  
+}
 
+void moveToSixthBase(){
+  followLine();
+  float right = voltPhotoRight();
+  float left = voltPhotoLeft();
+  /*if (left<0.05 && right<0.05) {
+    turnAngle(180.0);
+  }*/
+}
+
+void moveToFifthBase(){
+  followLine();
+  float right = voltPhotoRight();
+  float left = voltPhotoLeft();
+  if (left<0.04 && right<0.04) {
+    moveForwardCM(9);
+    exit(0);
+  }
+}
+
+void moveToFourthBase(){
+  followLine();
+  float right = voltPhotoRight();
+  float left = voltPhotoLeft();
+  if (left<0.09 && right<0.09) {
+    moveForwardCM(22);
+    time=millis()-beginTime-virageTime;
+    Serial.print("Distance parcourue = ");
+    float distance = time*0.001*11;
+    Serial.print(distance);
+    Serial.println(" cm");
+    delay(100);
+    exit(0);
+  }
+}
+
+void moveToThirdBase(){
+  blackLine();
+  if(i<BLACKLINES){
+    Serial.print("Distance parcourue = ");
+    Serial.println(j*0.25);
+  } 
 }
 
 // followLine with PID mechanics
 void followLine() {
 
   error = (float) linePosition();
-
+  
+  int virageTimeTmp1 = millis();
+  
   // too much left or too much right
   if(error > 0.13) {
     turnRight();
   } 
   else if(error < -0.13){
     turnLeft();
-  } 
+  }
+  
+  int virageTimeTmp2 = millis();
+  
+  virageTime = virageTime + virageTimeTmp2 - virageTimeTmp1;
 
   pV = kP * error + kD * (error - lastError);
   lastError = error;
@@ -216,15 +282,18 @@ void turnAngle(float angle) {
   rightServo.write(stopSign);
 }
 
-void moveToFirstBase(float x1, float y1, float x2, float y2) {
+void moveToSecondBase(float x1, float y1, float x2, float y2) {
   float alpha = atan(fabs(y2-y1)/fabs(x2-x1))*180/M_PI;
   if(x2>=x1 && y2>=y1){
     alpha = 90-alpha;
-  } else if (x2>=x1 && y2<y1){
+  } 
+  else if (x2>=x1 && y2<y1){
     alpha = 90 + alpha;
-  }  else if (x2<x1 && y2<y1){
+  }  
+  else if (x2<x1 && y2<y1){
     alpha = 360 - (90 + alpha);
-  }  else if (x2<x1 && y2>=y1){
+  }  
+  else if (x2<x1 && y2>=y1){
     alpha = 360 - (90 - alpha);
   }
   float distance = sqrt(pow(x1-x2, 2)+pow(y1-y2, 2));
@@ -257,6 +326,25 @@ void outputPhotoTransistorValues() {
   Serial.print("  ");
   Serial.println(voltPhotoRight());
   delay(10);
+}
+
+void blackLine() {
+  float photoResistanceRight1 = voltPhotoRight();
+  float photoResistanceLeft1 = voltPhotoLeft(); 
+  if (i<BLACKLINES) {
+    moveForwardCM(0.25);
+    j++;
+  }
+  float photoResistanceRight2 = voltPhotoRight();
+  float photoResistanceLetf2 = voltPhotoLeft();
+  float right = photoResistanceRight1 - photoResistanceRight2 ; 
+  float left = photoResistanceLeft1 - photoResistanceLetf2 ; 
+  float average = (left + right)/2 ;
+  if (average<-0.02) {
+    i++;
+    Serial.println(i);
+    Serial.println(average);
+  }
 }
 
 
